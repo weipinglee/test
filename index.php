b12c9ba3df6f200c5440950107405405bd1d6e1f
weipinglee <weiping.lee@163.com>
2018-05-11 10:27:59 +0800

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// phpinfo();exit;
+date_default_timezone_set('Asia/Shanghai');
+// error_reporting(E_ALL);
+define('APPLICATION_PATH', __DIR__);
+
+header("Content-Type:text/html;charset=utf-8");
+header('X-Frame-Options: SAMEORIGIN');
+$application = new Yaf\Application( APPLICATION_PATH . "/conf/application.ini");
+
+$application->bootstrap()->run();
+?>
